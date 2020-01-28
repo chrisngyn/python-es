05337396c5383471cd484ddac80dab89439a0943
Christopher Nguyen <chrisngyn99@gmail.com>
2020-01-28 14:07:57 -0500

    Finished

--- Numbers.py
+++ Numbers.py
@@ -1,1 +1,7 @@
 # Create a program that prompts a user for a circle radius and output circumference and area
+
+import math
+
+radius = float(input("Enter radius: ")) # don't forget to cast.
+print ("Circumference is: ", math.pi * radius * 2)
+print ("Area is: ", (math.pi * (radius ** 2)))
